6a2fc3e44200166d905901555e6dda98b4b891a9
YuugouOhno <yuugou.ohno@gmail.com>
2024-12-25 10:38:13 +0900

    格子線の追加

--- src/app/three/ThreeScene.tsx
+++ src/app/three/ThreeScene.tsx
@@ -4,18 +4,26 @@ import { Box } from './Shapes/Box';
 import { Earth } from './Stars/Earth';
 import { Sun } from './Stars/Sun';
 import { Moon } from './Stars/Moon';
+import { Grid } from './Environment/Grid';
 
 export function ThreeScene() {
     return (
         <Canvas 
             className='absolute inset-0'
+            camera={{ position: [5, 5, 5], fov: 75 }}
         >
-            <Box position={[-2, 0, 0]} color="red"/>
+            {/* ライト */}
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[5, 5, 5]} intensity={1} />
+            
+            {/* 格子線を追加 */}
+            <Grid size={100} divisions={10}/>
+
+            {/* 星を追加 */}
             <Earth position={[0, 0, 0]}/>
             <Sun position={[-5, 0, 0]}/>
             <Moon position={[3, 0, 0]}/>
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[5, 5, 5]} intensity={1} />
+            
         </Canvas>
     )
 }
